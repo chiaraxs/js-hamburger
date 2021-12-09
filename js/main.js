c61521084d894f51467b3059f914abc8eca451d0
chiaraxs <chiaracardinale92@gmail.com>
2021-12-09 16:34:36 +0100

    prova burger - 2

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@
 // 2. creo variabile per classe fa-bars a cui assegno valore 'button' //
 // 3. creo variabile per classe close a cui assegno valore 'closemenu' //
 
-const hamburger = document.getElementsByClassName ('hamburger-menu');
-const button = document.getElementsByClassName ('fa-bars');
-const closeMenu = document.getElementsByClassName ('close');
+// const hamburger = document.getElementsByClassName ('hamburger-menu');
+// const button = document.getElementsByClassName ('fa-bars');
+// const closeMenu = document.getElementsByClassName ('close');
 
 
+// prova con queryselector //
+const hamburger = document.querySelector('.hamburger-menu');
+const button = document.querySelector('.fa-bars');
+const closeMenu = document.querySelector('.close');
 
 // avvio l'evento con click //
 
 button.addEventListener("click", function() { 
-    'hamburger-menu' = "display-block"
+    hamburger.classList.add('active')
 });
 
 closeMenu.addEventListener("click", function() { 
-'hamburger-menu' = "display-none"
+    hamburger.classList.remove('active')
 });
 
 
