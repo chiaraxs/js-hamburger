d5491dad89a13d048e38c5e90234b190c4191e30
chiaraxs <chiaracardinale92@gmail.com>
2021-12-09 16:42:32 +0100

    added querySelector

--- js/main.js
+++ js/main.js
@@ -1,24 +1,19 @@
 // 1. creo variabile per classe hamburger-menu //
-// 2. creo variabile per classe fa-bars a cui assegno valore 'button' //
-// 3. creo variabile per classe close a cui assegno valore 'closemenu' //
+// 2. creo variabile per classe fa-bars a cui assegno valore 'openMenuButton' //
+// 3. creo variabile per classe close a cui assegno valore 'closeMenuButton' //
 
-// const hamburger = document.getElementsByClassName ('hamburger-menu');
-// const button = document.getElementsByClassName ('fa-bars');
-// const closeMenu = document.getElementsByClassName ('close');
-
-
-// prova con queryselector //
+// queryselector //
 const hamburger = document.querySelector('.hamburger-menu');
-const button = document.querySelector('.fa-bars');
-const closeMenu = document.querySelector('.close');
+const openMenuButton = document.querySelector('.fa-bars');
+const closeMenuButton = document.querySelector('.close');
 
 // avvio l'evento con click //
 
-button.addEventListener("click", function() { 
+openMenuButton.addEventListener("click", function() { 
     hamburger.classList.add('active')
 });
 
-closeMenu.addEventListener("click", function() { 
+closeMenuButton.addEventListener("click", function() { 
     hamburger.classList.remove('active')
 });
 
